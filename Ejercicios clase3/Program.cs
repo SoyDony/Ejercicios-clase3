945881690be9d31e74b527510d1e7aa08a7258bb
SoyDony <danielduran1516@gmail.com>
2025-04-05 13:29:56 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicios clase3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios_clase3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
